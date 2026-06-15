e4b55869ef7483d0c5c8d196324328e015774568
thodori <thodorikap@gmail.com>
2026-06-15 19:40:22 -0400

    ConVars to hide specific voicechat type

--- lua/autorun/client/main.lua
+++ lua/autorun/client/main.lua
@@ -8,43 +8,53 @@ if CLIENT then
 
 	-- Mostly the draw code copied from pure_skin_voice.lua with hooked in conditional logic for player type
 	function RestrictiveDraw()
-		if GetConVar("ttt2_hide_voice_panel"):GetInt() == 1 then
-			return
-		else
-			local elem = GetVoicePanel()
-			local client = LocalPlayer()
+		local hide_innocent = GetConVar("ttt2_hide_voice_panel_innocent"):GetInt() == 1
+		local hide_traitor = GetConVar("ttt2_hide_voice_panel_traitor"):GetInt() == 1
+		local hide_spectator = GetConVar("ttt2_hide_voice_panel_spectator"):GetInt() == 1
 
-			local pos = elem:GetPos()
-			local size = elem:GetSize()
-			local x, y = pos.x, pos.y
-			local w, h = size.w, size.h
+		local elem = GetVoicePanel()
+		local client = LocalPlayer()
 
-			local plys = player.GetAll()
-			local plysSorted = {}
+		local pos = elem:GetPos()
+		local size = elem:GetSize()
+		local x, y = pos.x, pos.y
+		local w, h = size.w, size.h
 
-			for i = 1, #plys do
-				local ply = plys[i]
+		local plys = player.GetAll()
+		local plysSorted = {}
 
-				if not VOICE.IsSpeaking(ply) then
-					continue
-				end
+		for i = 1, #plys do
+			local ply = plys[i]
 
-				if ply == client then
-					table.insert(plysSorted, 1, ply)
-
-					continue
-				end
-
-				plysSorted[#plysSorted + 1] = ply
+			if not VOICE.IsSpeaking(ply) then
+				continue
 			end
 
-			for i = 1, #plysSorted do
-				local ply = plysSorted[i]
+			if ply == client then
+				table.insert(plysSorted, 1, ply)
 
-				elem:DrawVoiceBar(ply, x, y, w, h)
-
-			y = y + h + elem.padding
+				continue
 			end
+
+			plysSorted[#plysSorted + 1] = ply
+		end
+
+		for i = 1, #plysSorted do
+			local ply = plysSorted[i]
+
+			if hide_innocent and ply.voiceMode == VOICE_MODE_GLOBAL then
+				continue
+			end
+			if hide_traitor and ply.voiceMode == VOICE_MODE_TEAM then
+				continue
+			end
+			if hide_spectator and ply.voiceMode == VOICE_MODE_SPEC then
+				continue
+			end
+
+			elem:DrawVoiceBar(ply, x, y, w, h)
+
+		y = y + h + elem.padding
 		end
 	end
 
@@ -54,13 +64,9 @@ if CLIENT then
 		if not elem then
 			print("TTT1-Permanent-Hide-Voice-Panel: pure_skin_voice not found")
 			return
-		else
-			PrintTable(elem) -- prints the table content
 		end
 
-		if GetConVar("ttt2_hide_voice_panel"):GetInt() == 1 then
-			elem.Draw = RestrictiveDraw
-		end
+		elem.Draw = RestrictiveDraw
 	end
 
 	--- PostInitPostEntity is called after all TTT1 HUD elements are initialized and mounted.
